c5e03957b341f218f758fc2db82c2eba8f201ef2
kalyankashaboina <kalyankashaboina07@gmail.com>
2024-04-26 15:53:37 +0530

    adding routes

--- controller/employeecontroler.js
+++ controller/employeecontroler.js
@@ -1,44 +1,104 @@
 
 
-const Employee=require("../model/employee")
+   const Employee=require("../model/employee")
 
-const createEmployee=async(req,res)=>{
-try{
-
-const{name,email,phone,city}=req.body
-    const employee=new Employee({
-    name,
-    email,
-    phone,
-    city
-    })
-
-    await employee.save()
-    res.status(201).json(employee)
-
-}
-catch(err){
-    console.log("message",err)
-    res.status(500).json({message:"server error"})
-}
-}
+        const createEmployee=async(req,res)=>{
+        try{
 
 
-const getAllEmployes=async(req,res)=>{
-    try{
-const employees=await Employee.find()
-res.status(201).json(employees)
-    }
-    catch(err){
-console.log("we got error",err)
-res.status(500).json({message:"error in getting data"})
-    }
-}
+        // CREATING OR INSERTING DATA TO MANGO
 
 
 
+        const{name,email,phone,city}=req.body
+            const employee=new Employee({
+            name,
+            email,
+            phone,
+            city
+            })
+
+            await employee.save()
+            res.status(201).json(employee)
+
+        }
+        catch(err){
+            console.log("message",err)
+            res.status(500).json({message:"server error"})
+        }
+        }
+
+
+        // GEtting all DATA
+
+        const getAllEmployes=async(req,res)=>{
+            try{
+        const employees=await Employee.find()
+        res.status(201).json(employees)
+            }
+            catch(err){
+        console.log("we got error",err)
+        res.status(500).json({message:"error in getting data"})
+            }
+        }
+
+
+        // GETTING SINGLE DATA
+
+
+
+        const singleEmployee=async(req,res)=>{
+
+        try{
+        const employee=await Employee.findById(req.params.id)
+
+
+        if(!employee){
+            return res.status(404).json({message:"user not found"})
+        }
+        res.status(200).json(employee)
+
+        }
+
+        catch(err){
+            console.log("server error",err)
+            res.status(500).json({message:"error getting single data"})
+        }
+
+        }
+
+
+        // Upadating Data
+
+
+
+        const updateEmployee=async(req,res)=>{
+
+        try{
+            const {name,email,phone,city}=req.body;
+
+        const employee=await Employee.findByIdAndUpdate(
+            req.params.id,
+            {name,email,phone,city}
+        ) 
+        if(!employee){
+            return res.status(404).json({mesage:"user not found"})
+        }
+        res.status(200).json(employee)
+
+
+        }
+        catch(err){
+            console.log("server error",err)
+            res.status(500).json({message:"server error uodating data"})
+        }
+
+        }
 
 
 
 
-module.exports={createEmployee,getAllEmployes}
+
+
+
+        module.exports={createEmployee,getAllEmployes,singleEmployee,updateEmployee}
--- routes/routes.js
+++ routes/routes.js
@@ -7,6 +7,8 @@ const Employee=require("../model/employee")
 
 router.post("/add_emp",employeeControler.createEmployee)
 router.get("/get_emp",employeeControler.getAllEmployes)
+router.get("/single_emp/:id",employeeControler.singleEmployee)
+router.put("/update_emp/:id",employeeControler.updateEmployee)
 
 
 module.exports=router
